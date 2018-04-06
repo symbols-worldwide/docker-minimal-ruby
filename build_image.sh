83deee402b80316b4038d59751f3018b83f7ba28
Simon Detheridge <simon@widgit.com>
2018-04-06 16:20:30 +0100

    Improve build script and handle newer rubies/alpines

--- build_image.sh
+++ build_image.sh
@@ -1,27 +1,64 @@
 #!/bin/bash
 
-set -e
-
 rvm get head
 rvm reload
 
-LATEST=2.4
+LATEST=2.5
 REPO=symbols/minimal-ruby
 
-for RUBY in `rvm list known_strings | grep '^ruby-2\.'` 
+for RUBY in `rvm list known_strings | grep '^ruby-2\.'`
 do
   MINOR=${RUBY#*-}
   MAJOR=${MINOR%.*}
+  POINT=${RUBY##*.}
 
-  if [ "$MAJOR" != "2.0" ]; then
+  ALPINES="3.7 3.6 3.5 3.4"
+  LATEST_ALPINE="3.7"
+  LAST_ALPINE=$LATEST_ALPINE
+  LAST_MAJOR=""
+
+  while [ "x$POINT" != "x-1" ]
+  do
     cp Dockerfile.template Dockerfile
     sed -i "s/%RUBY%/${MINOR}/" Dockerfile
-    docker build --pull --rm -t ${REPO}:${MINOR} -t ${REPO}:${MAJOR} -t ${REPO}:${MINOR}_3.5 -t ${REPO}:${MAJOR}_3.5 .
-    sed -i "s/alpine:3.5/alpine:3.4/" Dockerfile
-    docker build --pull --rm -t ${REPO}:${MINOR}_3.4 -t ${REPO}:${MAJOR}_3.4 .
-  fi
+  
+    for ALPINE in $ALPINES
+    do
+      echo -e "\033[0;33m\033[1mBuilding Ruby $MINOR for Alpine $ALPINE\033[21m\033[34m"
+      sed -i "s/alpine:$LAST_ALPINE/alpine:$ALPINE/" Dockerfile
+      docker build --pull --rm -t ${REPO}:${MINOR}_${ALPINE} . >> log/build_${MINOR}_${ALPINE}.log 2>&1
+      if [ "x$?" == "x0" ]; then
+        if [ "$ALPINE" == "$LATEST_ALPINE" ]; then
+          echo "- tagging ${REPO}:${MINOR}_${ALPINE} as ${REPO}:${MINOR}"
+          docker tag ${REPO}:${MINOR}_${ALPINE} ${REPO}:${MINOR}
+        fi
+        if [ "$LAST_MAJOR" != "$MAJOR" ]; then
+          echo "- tagging ${REPO}:${MINOR}_${ALPINE} as ${REPO}:${MAJOR}_${ALPINE}"
+          docker tag ${REPO}:${MINOR}_${ALPINE} ${REPO}:${MAJOR}_${ALPINE}
+          if [ "$ALPINE" == "$LATEST_ALPINE" ]; then
+            echo "- tagging ${REPO}:${MAJOR}_${ALPINE} as ${REPO}:${MAJOR}"
+            docker tag ${REPO}:${MAJOR}_${ALPINE} ${REPO}:${MAJOR}
+          fi
+        fi
+        echo -e "\033[0;32m- Building Ruby $MINOR for Alpine $ALPINE: \033[1mSUCCESS\033[21m"
+      else
+        echo -e "\033[0;31m- Building Ruby $MINOR for Alpine $ALPINE: \033[1mFAILED\033[21m"
+      fi
+      LAST_ALPINE=$ALPINE
+    done
+    LAST_MAJOR=$MAJOR
+    let POINT=$POINT-1
+    MINOR="${MAJOR}.${POINT}"
+  done
 done
+  
+if [ "x$DOCKERHUB_USERNAME" != "x" ]; then
+  echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
+fi
 
 docker tag ${REPO}:${LATEST} ${REPO}:latest
+echo -e "\033[0;34mAll done. Pushing changes."
 docker push ${REPO}
 
+echo -e "\033[1m\033[0;32m- Finished."
+echo -e "\033[0m"
